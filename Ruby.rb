cff733df6fe19887daae4bdba717680056d806ff
Furkan Yalçın <21060656@stu.omu.edu.tr>
2021-10-25 18:24:54 +0300

    İlkle

---
+++ Ruby.rb
@@ -0,0 +1,48 @@
+# Comment
+
+=begin
+
+**EKRANA YAZDIRMA**
+
+print "Naber?\n"
+print "İyiyim"
+
+or
+
+puts "Naber?"
+puts "İyiyim"  (direk alt alta yazdırmak için puts kullanılabilir.)
+
+=end
+
+=begin
+
+**ATAMALAR**
+
+a = "Furkan"
+b = 3
+c = 3.14
+
+isProcessing = true
+error = false
+noValue = nil
+
+=end
+
+=begin
+
+**ÖRNEK PROGRAM**
+
+print "First Number :"
+firstNumber = gets.to_i
+print "Second Number :"
+secondNumber = gets.to_i
+
+puts firstNumber.to_s + "+" + secondNumber.to_s + "=" + (firstNumber + secondNumber).to_s
+
+=end
+
+a = "Hello World!"
+puts a.length()
+puts a.index("r")
+puts a.index("H")
+puts a[0,4]
---
+++ ders.rb
@@ -0,0 +1,80 @@
+=begin
+    **ders dosyasi**
+=end
+
+=begin
+ 
+    puts 3.class --> ekrana "integer" yazdırır.
+
+    puts 3.14.class --> ekrana "float" yazdırır.
+
+    message = "Hello World!"
+    puts message.class --> ekrana "string" yazdırır.
+
+=end
+
+=begin
+
+    who = 'World'
+    message = "Hello #{who}"
+    puts message    -----------> Ekrana "Hello World" yazdırır.
+
+    Example:
+    print "Enter your age:"
+    yas = gets.to_i
+    puts "User's age is #{yas}"
+
+=end
+
+=begin
+    puts 'a'.ord ---> Ekrana bu karakterin ASCII tablosundaki sayısal karşılığını yazdırır.
+    puts '97'.chr ---> Ekrana bu sayının ASCII tablosundaki karakter karşılığını yazdırır.
+=end
+
+=begin
+
+print 'Your Name? '
+string = gets
+
+print "User's name is #{string}"
+
+=end
+
+=begin
+
+    ** STRING BIRLESTIRME **
+
+    string = ''
+    string << 'Furkan'
+    string << 'YALÇIN'      (String Birleştirme)
+    puts 'string' ---> Ekrana "Furkan YALÇIN" yazdırır.
+
+    Bu code bu haliyle çalışır. Lakin
+
+    # frozen_string_literal: true
+
+    string = ''
+    string << 'Furkan'
+    string << 'YALÇIN'
+    puts 'string'             (Hata verir.)
+
+    # frozen_string_literal: true --> Bu code kullanıldığında string birleştirmesi yapamayız. Hata alırız.
+
+=end
+
+#command İkinci dereceden bir bilinmeyenli denklemin köklerini hesaplayan program
+
+a=1.0
+b=5.0
+c=1.0
+
+delta = b ** 2 - 4 * a * c
+
+if (delta >= 0.0)
+    delta_sqrt = Math.sqrt(delta)
+    x1,x2 = (-b - delta_sqrt) / (2 * a), (-b + delta_sqrt) / (2 * a)
+    puts "Kökler #{x1.round(2)},#{x2.round(2)}"
+    
+else
+    puts "Kök yok."
+end
